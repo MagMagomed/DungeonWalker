2659ef02cd4181634e9064c80bec3a9b0ad6b454
LAPTOP-F0NQ82JO\mahen <mahendropolos@yandex.ru>
2024-03-24 14:15:27 +0300

    Добавьте файлы проекта.

---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MainCameraController : MonoBehaviour
+    {
+        [SerializeField] Transform _target;
+        [SerializeField] float _lookSpeed;
+        private Vector3 _offset;
+        private void Start()
+        {
+            _offset = transform.position - _target.position;
+        }
+        private void Update()
+        {
+            transform.LookAt(_target);
+            transform.Translate(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5.0f;
+    private Transform _cameraTransform;
+    private void Start()
+    {
+        // Get the camera transform
+        _cameraTransform = Camera.main.transform;
+    }
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+        transform.Translate(movement * _speed * Time.deltaTime);
+    }
+}
