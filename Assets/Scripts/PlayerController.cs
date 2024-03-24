3c55dd7fdfd2e8aa07c95d6a46afd46129e2b2bf
LAPTOP-F0NQ82JO\mahen <mahendropolos@yandex.ru>
2024-03-24 20:55:49 +0300

    Добавил спринт

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,10 +13,23 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        float shift = Input.GetAxisRaw("Fire3");
+        if (shift > 0) Run(moveHorizontal, moveVertical); else Walk(moveHorizontal, moveVertical);
+    }
+    private void Walk(float moveHorizontal, float moveVertical)
+    {
+        Move(moveHorizontal, moveVertical, _speed);
+    }
+    private void Run(float moveHorizontal, float moveVertical)
+    {
+        Move(moveHorizontal, moveVertical, _speed * 2);
+    }
+    private void Move(float moveHorizontal, float moveVertical, float speed)
+    {
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical).normalized;
         transform.rotation = new Quaternion(0, _cameraTransform.transform.rotation.y, 0, _cameraTransform.transform.rotation.w);
-        transform.Translate(movement * _speed * Time.deltaTime);
+        transform.Translate(movement * speed * Time.deltaTime);
     }
 }
