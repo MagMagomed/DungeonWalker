920aca1151be20d077b5bf8a95ab435a6ea41cc0
LAPTOP-F0NQ82JO\mahen <mahendropolos@yandex.ru>
2024-04-08 03:43:11 +0300

    Добавил прикосновение к стене, если она рядом.

---
+++ Assets/Scripts/Player/RigProceduralAnimation/HandAnimations/HandAnimation.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts.Player.RigProceduralAnimation.HandAnimations;
+using Cinemachine.Utility;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+using UnityEngine.Rendering.UI;
+
+namespace Assets.Scripts.Player.RigProceduralAnimation.HandAnimation
+{
+    public class HandAnimation : MonoBehaviour
+    {
+        [SerializeField] private Transform _handRig;
+
+        [SerializeField] private LayerMask _wallLayer;
+        [SerializeField] private float _maxDistance;
+        [SerializeField] private float _rayOffset;
+        [SerializeField] private Vector3 _rayDirection;
+
+        [SerializeField] private TwoBoneIKAnimation _twoBoneIKanimation;
+
+        private Ray _ray;
+        private void Update()
+        {
+            var rayTouchWall = false;
+            _ray = new Ray(_handRig.position + _handRig.forward * _rayOffset, _handRig.rotation * _rayDirection);
+            rayTouchWall = Physics.Raycast(_ray, out RaycastHit hitInfo, _maxDistance, _wallLayer, QueryTriggerInteraction.UseGlobal);
+
+            if (rayTouchWall) _twoBoneIKanimation.PlayAnimation(hitInfo.point);
+            else _twoBoneIKanimation.RevertAnimation();
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(_ray);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/RigProceduralAnimation/HandAnimations/TwoBoneIKAnimation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+namespace Assets.Scripts.Player.RigProceduralAnimation.HandAnimations
+{
+    public class TwoBoneIKAnimation : MonoBehaviour
+    {
+        [SerializeField] private TwoBoneIKConstraint _twoBoneIKContraint;
+        [SerializeField] private MultiRotationConstraint _multiRotationConstraint;
+
+        [SerializeField] private AnimationCurve _animationCurve;
+        [SerializeField] private float _duration;
+        private bool _animationInProgress;
+
+        public void PlayAnimation(Vector3 point)
+        {
+            _twoBoneIKContraint.transform.position = point;
+            if (!_animationInProgress && !IsAnimationEnded)
+            {
+                StartCoroutine(AnimationOnTime(_duration));
+            }
+        }
+        public void RevertAnimation()
+        {
+            if (!_animationInProgress && IsAnimationEnded)
+            {
+                StartCoroutine(AnimationOnTimeRevert(_duration));
+            }
+        }
+        private bool IsAnimationEnded => _twoBoneIKContraint.weight == 1 && _multiRotationConstraint.weight == 1;
+        private IEnumerator AnimationOnTime(float duration)
+        {
+            float exprideSeconds = 0;
+            float progress = 0;
+            float evaluted = 0f;
+            while (progress < 1f)
+            {
+                _animationInProgress = true;
+                exprideSeconds += Time.deltaTime;
+                progress = exprideSeconds / duration;
+                evaluted = _animationCurve.Evaluate(progress);
+
+                _twoBoneIKContraint.weight = evaluted;
+                _multiRotationConstraint.weight = evaluted;
+                yield return null;
+            }
+            _animationInProgress = false;
+        }
+        private IEnumerator AnimationOnTimeRevert(float duration)
+        {
+            float exprideSeconds = 0;
+            float progress = 0;
+            float evaluted = 0f;
+
+            float twoBoneIKContraintStartWeight = _twoBoneIKContraint.weight;
+            float multiRotationConstraintWeight = _multiRotationConstraint.weight;
+            while (progress < 1f)
+            {
+                _animationInProgress = true;
+                exprideSeconds += Time.deltaTime;
+                progress = exprideSeconds / duration;
+                evaluted = _animationCurve.Evaluate(progress);
+
+                _multiRotationConstraint.weight = multiRotationConstraintWeight - evaluted;
+                _twoBoneIKContraint.weight = twoBoneIKContraintStartWeight - evaluted;
+                yield return null;
+            }
+            _animationInProgress = false;
+        }
+    }
+}
